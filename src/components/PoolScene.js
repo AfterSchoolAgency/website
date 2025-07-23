b56c1534718e76822395734819f0571460df65bb
Your Name <you@example.com>
2025-07-22 22:06:41 -0700

    feat: Enlarge pool size for better layout

--- src/components/PoolScene.js
+++ src/components/PoolScene.js
@@ -345,7 +345,7 @@ export default function PoolScene() {
         }}
         onMouseLeave={() => setMousePos({ x: -1000, y: -1000 })}
       >
-        <div style={{ position: 'relative', width: '1000px', height: '300px' }}>
+        <div style={{ position: 'relative', width: '1050px', height: '320px' }}>
           {POOL_SVG}
           {/* Floaty letters are now positioned relative to THIS container */}
           <div className="floaties-container" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1 }}>
