e7707150293f573e638ea606ebc8971c6d9dfc5f
AfterSchoolAgency <contact@afterschoolagency.com>
2025-08-01 21:27:05 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import UnrollMenu from "@/components/UnrollMenu";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,12 +20,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <UnrollMenu />
         {children}
       </body>
     </html>
